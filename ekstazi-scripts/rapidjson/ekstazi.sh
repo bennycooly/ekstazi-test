c719053d2921a456858d3b947f577687debe509b
Ben Fu <ben_fu1@yahoo.com>
2018-04-14 14:50:16 -0500

    Add scripts for rapidjson

---
+++ ekstazi-scripts/rapidjson/ekstazi.sh
@@ -0,0 +1,4 @@
+cd build
+python $EKSTAZI_HOME/llvm-pass/ekstazi-test-runner/ekstazi-test-runner.py -d $PWD
+
+
---
+++ ekstazi-scripts/rapidjson/test.sh
@@ -0,0 +1,3 @@
+cd build
+python $EKSTAZI_HOME/llvm-pass/ekstazi-test-runner/ekstazi-test-runner.py -d $PWD -r
+
