31c92f468c4ad83fa2f201bd731371005c6a85e7
Ben Fu <ben_fu1@utexas.edu>
2018-05-16 14:55:28 -0500

    Update submodules

--- ekstazi-scripts/rapidjson/build.sh
+++ ekstazi-scripts/rapidjson/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p build
 cd build
-# CC=clang CXX=clang++ cmake -DCMAKE_C_FLAGS="-flto" -DCMAKE_CXX_FLAGS="-flto" -DCMAKE_CPP_FLAGS="-flto" -DCMAKE_EXE_LINKER_FLAGS="-flto -fuse-ld=gold -Wl,-plugin-opt=save-temps" ..
-# make -j8
+rm -f CMakeCache.txt
+CC=clang CXX=clang++ cmake -DCMAKE_C_FLAGS="-flto" -DCMAKE_CXX_FLAGS="-flto" -DCMAKE_CPP_FLAGS="-flto" -DCMAKE_EXE_LINKER_FLAGS="-flto -fuse-ld=gold -Wl,-plugin-opt=save-temps" ..
+make -j8
 
